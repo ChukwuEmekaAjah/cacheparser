21df115753cff46221abbf58486bb1e4b08375a2
ajahcs <72764511+ajahcs@users.noreply.github.com>
2020-11-03 00:37:48 +0100

    Add validity checkers for supported set commands

--- commands.go
+++ commands.go
@@ -1,5 +1,7 @@
 package cacheparser
 
+import "strconv"
+
 func isValidSetCommand(command string, arguments []string) bool {
 	if len(arguments) < 2 {
 		return false
@@ -16,18 +18,112 @@ func isValidGetCommand(command string, arguments []string) bool {
 	return true
 }
 
+func isValidSetexCommand(command string, arguments []string) bool {
+	if len(arguments) < 3 {
+		return false
+	}
+
+	_, err := strconv.ParseInt(arguments[1], 10, 64)
+
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func isValidSaddCommand(command string, arguments []string) bool {
+	if len(arguments) < 2 {
+		return false
+	}
+
+	return true
+}
+
+func isValidSetnxCommand(command string, arguments []string) bool {
+	if len(arguments) < 2 {
+		return false
+	}
+
+	return true
+}
+
+func isValidZaddCommand(command string, arguments []string) bool {
+	if len(arguments) < 3 {
+		return false
+	}
+
+	// member without score is part of the arguments
+	if len(arguments)%2 == 0 {
+		return false
+	}
+
+	return true
+}
+
+func isValidHsetCommand(command string, arguments []string) bool {
+	if len(arguments) != 3 {
+		return false
+	}
+
+	return true
+}
+
+func isValidLsetCommand(command string, arguments []string) bool {
+	if len(arguments) != 3 {
+		return false
+	}
+
+	_, err := strconv.ParseInt(arguments[1], 10, 64)
+
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func isValidLpushCommand(command string, arguments []string) bool {
+	if len(arguments) < 2 {
+		return false
+	}
+
+	return true
+}
+
+func isValidMsetCommand(command string, arguments []string) bool {
+	// should have an equal number of keys and values
+	if len(arguments)%2 == 1 {
+		return false
+	}
+	return true
+}
+
+func isValidHMSetCommand(command string, arguments []string) bool {
+	// should have at least one fiel-value pair with key name
+	if len(arguments) < 3 {
+		return false
+	}
+	// field without value part of arguments
+	if len(arguments)%2 == 0 {
+		return false
+	}
+
+	return false
+}
+
 // Commands lists all the functions this cache would support
 var Commands = map[string]func(commandKey string, arguments []string) bool{
 	"SET":     isValidSetCommand,
-	"SADD":    checkInputValidity,
-	"SETEX":   checkInputValidity,
-	"SETNX":   checkInputValidity,
-	"ZADD":    checkInputValidity,
-	"HSET":    checkInputValidity,
-	"LSET":    checkInputValidity,
-	"LPUSH":   checkInputValidity,
-	"MSET":    checkInputValidity,
-	"HMSET":   checkInputValidity,
+	"SADD":    isValidSaddCommand,
+	"SETEX":   isValidSetexCommand,
+	"SETNX":   isValidSetnxCommand,
+	"ZADD":    isValidZaddCommand,
+	"HSET":    isValidHsetCommand,
+	"LSET":    isValidLsetCommand,
+	"LPUSH":   isValidLpushCommand,
+	"MSET":    isValidMsetCommand,
+	"HMSET":   isValidHMSetCommand,
 	"GET":     isValidGetCommand,
 	"HGET":    checkInputValidity,
 	"HKEYS":   checkInputValidity,
