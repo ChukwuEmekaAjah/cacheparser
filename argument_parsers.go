751cfb9b73af7e539ad0677c7ee2f14bb1232b56
Chukwuemekaajah <talk2ajah@gmail.com>
2020-11-07 16:15:05 +0100

    Added more retrieval methods functions

--- argument_parsers.go
+++ argument_parsers.go
@@ -2,6 +2,7 @@ package cacheparser
 
 import (
 	"errors"
+	"fmt"
 )
 
 // KeyValue represents how we would store values internally
@@ -46,6 +47,46 @@ func retrieveGetFunction(commandName string, arguments []string, cacheMap map[st
 	return keyValue.value[0], nil
 }
 
+func retrieveExistsFunction(commandName string, arguments []string, cacheMap map[string]*KeyValue) (string, error) {
+	_, exists := cacheMap[arguments[0]]
+
+	if exists == false {
+		return "", errors.New("Unable to find key")
+	}
+
+	return "1", nil
+}
+
+func retrieveHlenFunction(commandName string, arguments []string, cacheMap map[string]*KeyValue) (string, error) {
+	value, exists := cacheMap[arguments[0]]
+
+	if exists == false {
+		return "", errors.New("Unable to find key")
+	}
+
+	return fmt.Sprint(len(value.value) / 2), nil
+}
+
+func retrieveLlenFunction(commandName string, arguments []string, cacheMap map[string]*KeyValue) (string, error) {
+	value, exists := cacheMap[arguments[0]]
+
+	if exists == false {
+		return "", errors.New("Unable to find key")
+	}
+
+	return fmt.Sprint(len(value.value)), nil
+}
+
+func retrieveStrlenFunction(commandName string, arguments []string, cacheMap map[string]*KeyValue) (string, error) {
+	value, exists := cacheMap[arguments[0]]
+
+	if exists == false {
+		return "", errors.New("Unable to find key")
+	}
+
+	return fmt.Sprint(len(value.value[0])), nil
+}
+
 // RetrievalFunctions lists all the functions this cache would support for retrieving values
 var RetrievalFunctions = map[string]func(commandKey string, arguments []string, cacheMap map[string]*KeyValue) (string, error){
 	"GET":     retrieveGetFunction,
@@ -55,14 +96,14 @@ var RetrievalFunctions = map[string]func(commandKey string, arguments []string, 
 	"LINDEX":  retrieveGetFunction,
 	"GETSET":  retrieveGetFunction,
 	"HGETALL": retrieveGetFunction,
-	"HLEN":    retrieveGetFunction,
+	"HLEN":    retrieveHlenFunction,
 	"HMGET":   retrieveGetFunction,
 	"PING":    retrieveGetFunction,
 	"HEXISTS": retrieveGetFunction,
-	"EXISTS":  retrieveGetFunction,
-	"LLEN":    retrieveGetFunction,
+	"EXISTS":  retrieveExistsFunction,
+	"LLEN":    retrieveLlenFunction,
 	"MGET":    retrieveGetFunction,
-	"STRLEN":  retrieveGetFunction,
+	"STRLEN":  retrieveStrlenFunction,
 	"ZCARD":   retrieveGetFunction,
 	"KEYS":    retrieveGetFunction,
 }
